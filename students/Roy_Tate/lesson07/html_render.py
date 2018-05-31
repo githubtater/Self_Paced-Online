cc40e8cf44f6f733ff1e56f61dac26a7cfff5054
tater <roytate@hotmail.com>
2018-05-30 21:34:09 -0700

    step6 working

--- students/Roy_Tate/lesson07/html_render.py
+++ students/Roy_Tate/lesson07/html_render.py
@@ -3,28 +3,93 @@
 ## Student:  Roy Tate (githubtater)
 
 
-class Element:
-    tagname = 'html'
-    indent = ' '
 
-    def __init__(self, content=None, *args):
+class Element:
+    tag = ''
+    indent = '    '  # 4 spaces for indentation
+
+    def __init__(self, content=None, **kwargs):
         if content:
             self.content = [content]
         else:
             self.content = []
-        self.attributes = args
+        self.kwargs = kwargs
+
+    def append(self, content):
+            self.content.append(content)
+
+    def render(self, file_out, cur_ind=''):
+        file_out.write(cur_ind + '<{}'.format(self.tag))
+        file_out.write('>\n')
+        for item in self.content:
+            if isinstance(item, Element):
+                item.render(file_out, cur_ind + self.indent)
+            else:
+                file_out.write(cur_ind + self.indent + item + '\n')
+        file_out.write(cur_ind + '</{}>\n'.format(self.tag))
 
 
-    def append(self, string=None):
-        self.content.append(string)
+class Html(Element):
+    tag='html'
 
-    def render(self, f):
-        pass
-
-
-class Html:
-    pass
+    def render(self, file_out, cur_ind=''):
+        file_out.write('<!DOCTYPE html>\n')
+        Element.render(self, file_out, cur_ind='')
 
 
-class Body:
-    pass
+class Title(Element):
+    tag = 'title'
+
+
+class Body(Element):
+    tag = 'body'
+
+
+class P(Element):
+    tag = 'p'
+
+
+class Head(Element):
+    tag = 'head'
+
+
+class OneLineTag(Element):
+
+    def render(self, file_out, cur_ind=''):
+        file_out.write(cur_ind + '<{}'.format(self.tag))
+        file_out.write('>\n')
+        for item in self.content:
+            if isinstance(item, Element):
+                item.render(file_out, cur_ind + self.indent)
+            else:
+                file_out.write(cur_ind + self.indent + item + '\n')
+        file_out.write(cur_ind + '</{}>\n'.format(self.tag))
+
+
+class SelfClosingTag(Element):
+    def __init__(self, content=None, **kwargs):
+        if content:
+            raise TypeError('Error: class SelfClosingTag does not take content')
+        self.kwargs = kwargs
+
+    def render(self, file_out, cur_ind=''):
+        file_out.write(cur_ind + '<{}'.format(self.tag))
+        for key, value in self.kwargs.items():
+            file_out.write(' {}="{}"'.format(key, value))
+        file_out.write(' />\n')
+
+
+class Hr(SelfClosingTag):
+    tag = 'hr'
+
+
+class Br(SelfClosingTag):
+    tag = 'br'
+
+
+class Ul(Element):
+    tag = 'ul'
+
+
+class Li(Element):
+    tag = 'li'
--- students/Roy_Tate/lesson07/test_html_render.py
+++ students/Roy_Tate/lesson07/test_html_render.py
@@ -5,18 +5,17 @@
 
 import unittest
 import html_render as hr
-
+from io import StringIO
 
 class TestHTMLRender(unittest.TestCase):
     test_string = '<<TEST_STRING_FOR_TEXT_INSERTION_5599>>'
 
     def test_verify_element_class_attributes(self):
         element_attr = hr.Element()
-        self.assertEqual(element_attr.tagname, 'html')
-        self.assertEqual(element_attr.indent, ' ')
+        self.assertEqual(element_attr.tag, '')
+        self.assertEqual(element_attr.indent, '    ')
 
-    def test_element_class_append(self):
-
+    def test_element_class_appends_string(self):
         element = hr.Element()
         element.append(self.test_string)
         self.assertEqual(element.content[-1], self.test_string)
@@ -25,6 +24,17 @@ class TestHTMLRender(unittest.TestCase):
         kwarg1 = '1'
         kwarg2 = 'first'
         element_attr = hr.Element(self.test_string, kwarg1, kwarg2)
-        self.assertEquals(element_attr.attributes, (kwarg1, kwarg2))
+        self.assertEquals(element_attr.args, (kwarg1, kwarg2))
+
+    def test_element_class_renders_correctly(self):
+        pass
 
 
+    def test_html_class(self):
+        new_html = hr.Html()
+        assert isinstance(new_html, hr.Html)
+
+
+    def test_body_class(self):
+        new_body = hr.Body()
+        assert isinstance(new_body, hr.Body)
