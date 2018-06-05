ea5781d2ed3926954936e53e7b83b6b8268940d5
tater <roytate@hotmail.com>
2018-06-04 21:39:09 -0700

    commit

--- students/Roy_Tate/lesson07/test_html_render.py
+++ students/Roy_Tate/lesson07/test_html_render.py
@@ -1,5 +1,5 @@
 
-#!/usr/bin/python3.6 pytest
+#!/usr/bin/python3 pytest
 
 # Author: Roy Tate (githubtater)
 
@@ -8,33 +8,68 @@ import html_render as hr
 from io import StringIO
 
 class TestHTMLRender(unittest.TestCase):
-    test_string = '<<TEST_STRING_FOR_TEXT_INSERTION_5599>>'
+    item_one = 'This is item number 1'
+    item_two = 'This is item number 2'
 
     def test_verify_element_class_attributes(self):
         element_attr = hr.Element()
         self.assertEqual(element_attr.tag, '')
         self.assertEqual(element_attr.indent, '    ')
 
-    def test_element_class_appends_string(self):
+    def test_element_class_renders_content(self):
+        item_one = 'This is item number 1'
         element = hr.Element()
-        element.append(self.test_string)
-        self.assertEqual(element.content[-1], self.test_string)
+        element.append(self.item_one)
+        self.assertEqual(element.content[-1], self.item_one)
 
-    def test_element_class_accepts_kwargs(self):
-        kwarg1 = '1'
-        kwarg2 = 'first'
-        element_attr = hr.Element(self.test_string, kwarg1, kwarg2)
-        self.assertEquals(element_attr.args, (kwarg1, kwarg2))
 
-    def test_element_class_renders_correctly(self):
-        pass
-
+    def test_onelinetag_class_renders_content(self):
+        test_tag = hr.OneLineTag('Test Title Test')
+        test_tag.tag = 'title'
+        test_tag.indent = ''
+        output = StringIO()
+        test_tag.render(output)
+        self.assertEqual(output.getvalue(), '<title>\nTest Title Test\n</title>\n')
 
     def test_html_class(self):
         new_html = hr.Html()
-        assert isinstance(new_html, hr.Html)
+        self.assertEqual(new_html.tag, 'html')
+        output = StringIO()
+        new_html.render(output)
+        self.assertEqual(output.getvalue(), '<!DOCTYPE html>\n<html>\n</html>\n')
 
+    def test_unordered_list_generates_correctly(self):
+        ul_test = hr.Ul(self.item_one)
+        output = StringIO()
+        ul_test.render(output)
+        self.assertEqual(output.getvalue(), '<ul>\n    This is item number 1\n</ul>\n')
 
-    def test_body_class(self):
-        new_body = hr.Body()
-        assert isinstance(new_body, hr.Body)
+    def test_href_link_generates_correctly(self):
+        href_link = hr.A('www.python.org', 'link to python.org')
+        href_link.indent = ''
+        output = StringIO()
+        href_link.render(output)
+        self.assertEqual(output.getvalue(), '<a href="www.python.org">\nlink to python.org\n</a>\n')
+
+    def test_h_tag_returns_correct_size(self):
+        header = hr.H(1, 'Head1')
+        header.indent = ''
+        output = StringIO()
+        header.render(output)
+        self.assertEqual(output.getvalue(), '<h1>\nHead1\n</h1>\n')
+
+    def test_self_closing_tag(self):
+        sc_tag = hr.SelfClosingTag()
+        sc_tag.tag = 'br'
+        output = StringIO()
+        sc_tag.render(output)
+        self.assertEqual(output.getvalue(), '<br />\n')
+
+        sc_tag_attr = hr.SelfClosingTag(charset='UTF-8')
+        sc_tag_attr.tag = 'meta'
+        output = StringIO()
+        sc_tag_attr.render(output)
+        self.assertEqual(output.getvalue(), '<meta charset="UTF-8" />\n')
+
+if __name__ == "__main__":
+    unittest.main()
